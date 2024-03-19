96c2fcb1e437c3661d275c372587f433bb5f341b
v.m.work <v.melnyk@mychip.io>
2024-03-19 16:44:16 +0200

    #1 Add core
    Add core logic with auth metamask

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+export default function Home() {
+  const { account, connectWallet, error } = useContext(AppContext);
+
+  console.log(error);
+
+  return (
+    <div className='container'>
+      <div className='box'>
+        <h2>
+          MetaMask <span className='block'>Connect.</span>
+        </h2>
+
+        {account ? (
+          <div className='account-box'>
+            <p className='shadow-border'>{account}</p>
+          </div>
+        ) : (
+          <button className='btn shadow-border' onClick={connectWallet}>
+            Connect
+          </button>
+        )}
+        {error && <p className={`error shadow-border`}>{`Error: ${error}`}</p>}
+      </div>
+    </div>
+  );
+}
